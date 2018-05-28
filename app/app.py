cd8ae7a0300a9af6ce5711a80e3fed62ea07289d
Rodrigo Hernandez <rodrhern@gmail.com>
2018-05-28 16:38:10 -0400

    app

---
+++ app/app.py
@@ -0,0 +1,81 @@
+import io
+import json
+import logging.handlers
+
+import cv2
+import facerec.detector
+import middleware
+import numpy as np
+import utils
+from flask import Flask, request
+
+log_file_name = './logs/sentinel.log'
+logging_level = logging.DEBUG
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s")
+handler = logging.handlers.TimedRotatingFileHandler(log_file_name, when="midnight", backupCount=10)
+handler.setFormatter(formatter)
+
+logger = utils.SentinelLogger(__name__)
+logger.addHandler(handler)
+logger.setLevel(logging_level)
+
+app = Flask(__name__)
+app.wsgi_app = middleware.TokenRequiredMiddleware(app.wsgi_app)
+
+
+def decode_image(image, colorspace=''):
+    in_memory_file = io.BytesIO()
+    image.save(in_memory_file)
+    data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+    if colorspace == 'gray':
+        pix = cv2.imdecode(data, cv2.IMREAD_GRAYSCALE)
+    else:
+        img = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        pix = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    print(pix.shape)
+    return pix
+
+
+@app.route('/', methods=['GET', ])
+def index():
+    return json.dumps({'status': 200, 'facenet_api': 'v1.0'}), 200
+
+
+@app.route('/face_detector/', methods=['POST', ])
+def face_detector():
+    try:
+        # load request data
+        params = json.loads(request.form.get('params'))
+
+        # get file
+        image = request.files.get('file')
+        if not image:
+            logger.error('image not sent')
+            return json.dumps({'status': 400, 'error': 'file not sent'}), 400
+
+        # parse detector inputs
+        opts = {
+            'detector': params.pop('detector', 'mtcnn'),
+            'min_face_size': params.pop('min_face_size', 96),
+        }
+
+        if opts['detector'] in ['mtcnn', 'mtcnnY']:
+            opts.update({'threshold': params.get('threshold', [0.8, 0.7, 0.7])})
+            opts.update({'factor': params.get('factor', 0.5)})
+
+        logger.info("Face detector", **opts)
+
+        colorspace = params.pop('colorspace', 'rgb')
+        pix = decode_image(image, colorspace=colorspace)
+        detections, scores, landmarks = facerec.detector.face_detector(pix, **opts)
+        response = {'detections': detections, 'scores': scores, 'landmarks': landmarks}
+
+        return json.dumps({'status': 200, 'data': response}), 200
+    except:
+        logger.exception()
+        return json.dumps({'status': 400, 'error': 'Bad Request'}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/tests/test.py
@@ -0,0 +1,81 @@
+import os
+import requests
+import time
+import json
+import unittest
+from multiprocessing import Process
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_DIR = os.path.join(TESTS_DIR, 'resources')
+
+API_URL_BASE = 'http://localhost:5000/{}/'
+API_TOKEN = os.environ.get('API_TOKEN', 'facerec_api_dev_token')
+
+FACE_DETECTOR_URL = API_URL_BASE.format('face_detector')
+
+FACE_DETECTOR_TYPE = 'mtcnn'
+MIN_FACE_SIZE = 96
+
+
+def face_detect(img, id='', detector=FACE_DETECTOR_TYPE, min_face_size=MIN_FACE_SIZE, colorspace='rgb'):
+    params = {
+        'id': str(id), 
+        'detector': detector, 
+        'min_face_size': min_face_size, 
+        'colorspace': colorspace,
+    }
+    payload = {'params': json.dumps(params)}
+    files = {'file': img}
+    headers = {'AUTHENTICATION': API_TOKEN}
+    r = requests.post(FACE_DETECTOR_URL, data=payload, files=files, headers=headers)
+    return r.json()
+
+
+class ApiRequestsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.speed_iterations_number = 100
+
+    def test_face_detection_success_mtcnn(self):
+        rick_path = os.path.join(RESOURCES_DIR, 'rick.jpg')
+        response = face_detect(open(rick_path, 'rb'), detector='mtcnn')
+        self.assertEqual(response['status'], 200)
+        self.assertEqual(len(response['data']['detections']), 6)
+
+    def test_face_detection_success_mtcnnY(self):
+        rick_path = os.path.join(RESOURCES_DIR, 'rickY.jpg')
+        response = face_detect(open(rick_path, 'rb'), detector='mtcnnY', colorspace='gray')
+        self.assertEqual(response['status'], 200)
+        self.assertEqual(len(response['data']['detections']), 6)
+
+    def test_face_detection_speed_mtcnn(self):
+        rick_path = os.path.join(RESOURCES_DIR, 'rick.jpg')
+        start = time.time()
+        procs = []
+        for i in range(self.speed_iterations_number):
+            procs.append(Process(target=face_detect, args=(open(rick_path, 'rb'),), kwargs={'detector':'mtcnn'}))
+        for p in procs:
+            p.start()
+        for p in procs:
+            p.join()
+        delta = time.time() - start
+        avg_fps = self.speed_iterations_number/delta
+        avg_time = 1000.0/avg_fps
+        print('face_detect [mtcnn]:: avg time: {:.5f} ms. fps: {:.5f}'.format(avg_time, avg_fps))
+        self.assertTrue(avg_time < 300)
+
+    def test_face_detection_speed_mtcnnY(self):
+        rick_pathY = os.path.join(RESOURCES_DIR, 'rickY.jpg')
+        start = time.time()
+        procs = []
+        for i in range(self.speed_iterations_number):
+            procs.append(Process(target=face_detect, args=(open(rick_pathY, 'rb'),), kwargs={'detector':'mtcnnY', 'colorspace': 'gray'}))
+        for p in procs:
+            p.start()
+        for p in procs:
+            p.join()
+        delta = time.time() - start
+        avg_fps = self.speed_iterations_number/delta
+        avg_time = 1000.0/avg_fps
+        print('face_detect [mtcnnY]:: avg time: {:.5f} ms. fps: {:.5f}'.format(avg_time, avg_fps))
+        self.assertTrue(avg_time < 300)
